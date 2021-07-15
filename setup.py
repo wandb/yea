1220edf7f626f16c6c5834d63a3ef361852d0cf9
Danny Goldstein <dgold@berkeley.edu>
2021-07-14 17:01:54 -0700

    bump version to 0.3.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name="yea",
-    version="0.3.0",
+    version="0.3.1",
     description="Test harness breaking the sound barrier",
     packages=["yea"],
     install_requires=[
