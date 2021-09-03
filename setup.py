1bbf0cbfc25368fef3868492db594c40b22adc20
Jeff Raubitschek <jeff@wandb.com>
2021-09-03 06:04:00 -0700

    bump 0.7.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name="yea",
-    version="0.7.2",
+    version="0.7.3",
     description="Test harness breaking the sound barrier",
     packages=["yea"],
     install_requires=[
