e2bc62dc089f4ce9807dfb7a763bb0de09d9625d
Jeff Raubitschek <jeff@wandb.com>
2021-11-17 21:58:00 -0800

    add pip timeouts

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name="yea",
-    version="0.7.5",
+    version="0.7.6",
     description="Test harness breaking the sound barrier",
     packages=["yea"],
     install_requires=[
