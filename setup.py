52c70a5ec8001e0339e820aca0329c0713a5ff72
Dmitry Duev <dmitry.duev@gmail.com>
2022-01-15 00:46:08 -0800

    version++

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name="yea",
-    version="0.7.9",
+    version="0.7.10",
     description="Test harness breaking the sound barrier",
     packages=["yea"],
     install_requires=[
