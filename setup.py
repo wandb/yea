wandb/yea

commit 2bd40597fc3d6a1c5b1e7c11ebef7ff4394f3bf8

Make plugins configurable at runtime + allow non-str values in parametrize/permute (#12)
* allow non-str values in parametrize/permute
* make plugins configurable when running tests
* add config and ytest utils tests

setup.py
@@ -6,15 +6,16 @@ from setuptools import setup
 
 setup(
     name="yea",
-    version="0.7.9",
+    version="0.7.10",
     description="Test harness breaking the sound barrier",
     packages=["yea"],
     install_requires=[
         "coverage",
-        "jsonschema",
         "importlib-metadata>=3.0.0",
+        "jsonschema",
         "junit-xml",
         "PyYAML",
+        "requests",
     ],
     package_dir={"": "src"},
     entry_points={

src/yea/_setup.py
@@ -34,7 +34,7 @@ def setup_plugins(params):
         mod_setup = getattr(mod, "setup", None)
         if not mod_setup:
             continue
-        mod_setup()
+        mod_setup(params)
 
 
 def setup():

.yearc
@@ -1,0 +1,10 @@
+[yea]
+
+test_paths =
+  tests/
+
+coverage_config_template = .coveragerc
+coverage_source = src/yea
+coverage_source_env = YEACOV_SOURCE
+
+results_file = test-results/junit-yea.xml

src/yea/config.py
@@ -4,6 +4,7 @@ import configparser
 import os
 import re
 from pathlib import Path
+from typing import List
 
 
 class Config:
@@ -24,7 +25,7 @@ class Config:
             self._cfroot = Path(".")
             self._test_dirs = ["."]
 
-    def _find_config(self) -> Path:
+    def _find_config(self) -> bool:
         p = Path.cwd()
         # TODO: change to use parents
         while True:
@@ -42,13 +43,13 @@ class Config:
             p = n
         return False
 
-    def _load_config(self):
+    def _load_config(self) -> configparser.ConfigParser:
         p = self._cfname
         cf = configparser.ConfigParser()
         cf.read(p)
         return cf
 
-    def _parse_config(self, cf):
+    def _parse_config(self, cf: configparser.ConfigParser) -> List[str]:
         ycfg = cf.items("yea")
         ydict = dict(ycfg)
         test_paths = ydict.get("test_paths", "")

src/yea/plugins.py
@@ -24,6 +24,11 @@ class Plugins:
             plug = m.init_plugin(self._yc)
             self._plugin_list.append(plug)
 
+    def get_plugin(self, name: str):
+        for p in self._plugin_list:
+            if p._name == name:
+                return p
+
     def monitors_inform(self, tlist):
         for p in self._plugin_list:
             for t in tlist:

src/yea/runner.py
@@ -63,7 +63,7 @@ class TestRunner:
         return alist
 
     def _get_dirs(self):
-        # generate to return all test dirs and rcursively found dirs
+        # generate to return all test dirs and recursively found dirs
         for tdir in self._cfg.test_dirs:
             path_dir = pathlib.Path(self._cfg.test_root, tdir)
             yield path_dir
@@ -117,7 +117,7 @@ class TestRunner:
                     # TODO: parse yea file looking for path info
                     spec = testspec.load_yaml_from_file(tpath)
 
-                    # if program is specied, keep track of yea file
+                    # if program is specified, keep track of yea file
                     py_fname = spec.get("command", {}).get("program")
                     if py_fname:
                         # hydrate to full path, take base from tpath
@@ -281,11 +281,11 @@ class TestRunner:
     def finish(self):
         self.clean()
         self._save_results()
-        exit = 0
+        exit_code = 0
         print("\nResults:")
         print("--------")
         if not self._results:
-            sys.exit(exit)
+            sys.exit(exit_code)
         tlen = max([len(tc.name) for tc in self._results])
         for tc in self._results:
             # TODO: fix hack that only looks at first message
@@ -296,8 +296,8 @@ class TestRunner:
 
             print("  {:<{}s}: {}{}".format(tc.name, tlen, emoji, r))
             if r:
-                exit = 1
-        sys.exit(exit)
+                exit_code = 1
+        sys.exit(exit_code)
 
     def get_tests(self):
         return self._test_list

src/yea/ytest.py
@@ -1,6 +1,7 @@
 """Yea test class."""
 
 import configparser
+import functools
 import itertools
 import json
 import os
@@ -8,15 +9,18 @@ import pathlib
 import subprocess
 import sys
 import time
+from typing import Any, Dict, List, Mapping
 
 import requests
 
 from yea import testcfg, testspec
 
 
-def run_command(cmd_list, timeout=None, env=None):
-    env = env or os.environ
-    timeout = timeout or 300
+def run_command(
+    cmd_list: List[str],
+    timeout: int = 300,
+    env: Mapping = os.environ,
+):
     print("INFO: RUNNING=", cmd_list)
     p = subprocess.Popen(cmd_list, env=env)
     try:
@@ -33,8 +37,8 @@ def run_command(cmd_list, timeout=None, env=None):
     return p.returncode
 
 
-def download(url, fname):
-    err = False
+def download(url: str, fname: str) -> bool:
+    err: bool = False
     print(f"INFO: grabbing {fname} from {url}")
     try:
         with requests.get(url, stream=True) as r:
@@ -46,6 +50,62 @@ def download(url, fname):
         print("ERROR: url download error", url, e)
         err = True
     return err
+
+
+def get_config(config: Dict[str, Any], prefix: str) -> Dict[str, Any]:
+    """
+    Recursively parse a "flat" config with column-separated key name definitions
+    into a nested dictionary given a prefix.
+
+    Example:
+        config = {
+            ":rug:ties_room_together": True,
+            ":wandb:mock_server:lol": True,
+            ":wandb:mock_server:lmao": False,
+            ":wandb:mock_server:resistance:object": "Borg",
+            ":wandb:mock_server:resistance:is_futile": True,
+            ":wandb:foo": "bar",
+        }
+        prefix = ":wandb:"
+        get_config(config, prefix)
+        # {
+        #     "mock_server": {
+        #         "lol": True,
+        #         "lmao": False,
+        #         "resistance": {
+        #             "object": "Borg",
+        #             "is_futile": True,
+        #         },
+        #     },
+        #     "foo": "bar",
+        # }
+
+    """
+    # recursively get config values
+    def parse(key: str, value: Any):
+        if ":" not in key:
+            return {key: value}
+        else:
+            key, subkey = key.split(":", 1)
+            return {key: parse(subkey, value)}
+
+    # recursively merge configs
+    def merge(source: Dict[str, Any], destination: Dict[str, Any]):
+        for key, value in source.items():
+            if isinstance(value, dict):
+                # get node or create one
+                node = destination.setdefault(key, {})
+                merge(value, node)
+            else:
+                destination[key] = value
+        return destination
+
+    prefixed_items = {k[len(prefix) :]: v for (k, v) in config.items() if k.startswith(prefix)}
+    parsed_items = []
+    for k, v in prefixed_items.items():
+        parsed_items.append(parse(k, v))
+
+    return functools.reduce(merge, parsed_items) if parsed_items else {}
 
 
 class YeaTest:
@@ -145,17 +205,31 @@ class YeaTest:
             env.update(edict)
         if self._permute_groups and self._permute_items:
             env["YEA_PARAM_NAMES"] = ",".join(self._permute_groups)
-            env["YEA_PARAM_VALUES"] = ",".join(self._permute_items)
+            env["YEA_PARAM_VALUES"] = ",".join(map(str, self._permute_items))
 
         plugins = self._test_cfg.get("plugin", [])
+        if self._permute_groups and self._permute_items:
+            params = {k: v for (k, v) in zip(self._permute_groups, self._permute_items)}
+        else:
+            params = None
         if plugins:
-            for p in plugins:
-                penv = p.upper()
+            for plugin_name in plugins:
+                prefix = f":{plugin_name}:"
+
+                if params is not None:
+                    # need to configure the plugin?
+                    # get the plugin by its name
+                    plug = self._yc._plugs.get_plugin(plugin_name)
+                    plugin_params = get_config(params, prefix)
+                    if plugin_params:
+                        plug.monitors_configure(plugin_params)
+
+                # process vars
+                penv = plugin_name.upper()
                 pnames = []
                 pvalues = []
                 for items in self._test_cfg.get("var", []):
                     for k, v in items.items():
-                        prefix = f":{p}:"
                         if k.startswith(prefix):
                             pnames.append(k[len(prefix) :])
                             pvalues.append(v)
@@ -163,7 +237,6 @@ class YeaTest:
                     env[f"YEA_PLUGIN_{penv}_NAMES"] = ",".join(pnames)
                     env[f"YEA_PLUGIN_{penv}_VALUES"] = json.dumps(pvalues)
             env["YEA_PLUGINS"] = ",".join(plugins)
-
         exit_code = run_command(cmd_list, env=env, timeout=timeout)
         self._retcode = exit_code
 
@@ -239,7 +312,7 @@ class YeaTest:
         self._prep()
         if not self._args.dryrun:
             err = self._depend()
-            # TODO: record error insted of assert
+            # TODO: record error instead of assert
             assert not err, "Problem getting test dependencies"
             self._time_start = time.time()
             self._run()
@@ -269,10 +342,10 @@ class YeaTest:
             t._load()
             t._permute_groups = gnames
             t._permute_items = it
-            tpname = "{}-{}".format(tnum, "-".join(it))
+            tpname = f"{tnum}-{'-'.join(map(str, it))}"
             tid = t._test_cfg.get("id")
             if tid:
-                t._test_cfg["id"] = "{}.{}".format(tid, tpname)
+                t._test_cfg["id"] = f"{tid}.{tpname}"
             r.append(t)
         return r
 

tests/config-test.yea
@@ -1,0 +1,45 @@
+id: 0.0.1
+name: example test
+tag:
+  suite: nightly
+env:
+  - this: that
+command:
+  timeout: 3
+  program: proc.py  # this defaults to the basename of yea file + ".py"
+  args:
+    - param1
+    - param2
+plugin:
+  - wandb
+depend:
+  requirements:
+    - wandb
+  files:
+    - file: this.txt
+      source: https://raw.githubusercontent.com/wandb/wandb-testing/master/README.md
+var:
+  - runs_len:
+      :fn:len: :wandb:runs
+  - run0:
+      :fn:find:
+        - item
+        - :wandb:runs
+        - :item[config][id]: 0
+assert:
+  - :runs_len: 1
+  - :op:contains:
+      - :run0[telemetry][3]  # feature
+      - 8  # keras
+  - :wandb:runs_len: 1
+  - :wandb:runs[0][config]: {id: 0}
+  - :wandb:runs[0][summary]:
+      m1: 1
+      m2: 2
+  - :wandb:runs[0][exitcode]: 0
+parametrize:
+  permute:
+    - :yea:start_method:
+        - fork
+        - spawn
+        - forkserver

commit 58602ebd41fb3e1f8046a4c10cebdf2a972ce78f

Fix bug in setup_plugins introduced in #12 (#13)
* fix setup_plugins
* version++

setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name="yea",
-    version="0.7.10",
+    version="0.7.11",
     description="Test harness breaking the sound barrier",
     packages=["yea"],
     install_requires=[

src/yea/_setup.py
@@ -34,7 +34,7 @@ def setup_plugins(params):
         mod_setup = getattr(mod, "setup", None)
         if not mod_setup:
             continue
-        mod_setup(params)
+        mod_setup()
 
 
 def setup():