c83895bd899184b003ed1584f32d0e6c9c93bfc7
Katia <katia@wandb.com>
2022-04-22 16:44:04 -0700

    bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name="yea",
-    version="0.7.15",
+    version="0.7.16",
     description="Test harness breaking the sound barrier",
     packages=["yea"],
     install_requires=[
