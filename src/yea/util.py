7112cc2f2d224efea1cc59544dc5cae750de2e5e
Jeff Raubitschek <jeff@wandb.com>
2022-08-14 12:26:29 -0700

    vendor junit xml (#35)

---
+++ src/yea/util.py
@@ -0,0 +1,36 @@
+import os
+import sys
+from importlib import import_module
+from typing import Any, Callable
+
+
+def vendor_setup() -> Callable:
+    """This enables us to use the vendor directory for packages we don't depend on
+    Returns a function to call after imports are complete. Make sure to call this
+    function or you will modify the user's path which is never good. The pattern should be:
+    reset_path = vendor_setup()
+    # do any vendor imports...
+    reset_path()
+    """
+    original_path = [directory for directory in sys.path]
+
+    def reset_import_path() -> None:
+        sys.path = original_path
+
+    parent_dir = os.path.abspath(os.path.dirname(__file__))
+    vendor_dir = os.path.join(parent_dir, "vendor")
+    vendor_packages = ("junit_xml",)
+    package_dirs = [os.path.join(vendor_dir, p) for p in vendor_packages]
+    for p in [vendor_dir] + package_dirs:
+        if p not in sys.path:
+            print("ADD", p)
+            sys.path.insert(1, p)
+
+    return reset_import_path
+
+
+def vendor_import(name: str) -> Any:
+    reset_path = vendor_setup()
+    module = import_module(name)
+    reset_path()
+    return module
---
+++ src/yea/vendor/junit_xml/wandb-vendor.md
@@ -0,0 +1,8 @@
+From:
+https://github.com/kyrus/python-junit-xml
+
+Commit:
+4bd08a272f059998cedf9b7779f944d49eba13a6
+
+Changes:
+- rename `junit_xml` dir to `wandb_junit_xml`
